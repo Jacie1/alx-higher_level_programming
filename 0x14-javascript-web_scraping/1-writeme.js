9209c8eb1451588726492ad91eb8e4b63b8cc175
Jacie1 <irinemwanga5@gmail.com>
2023-04-25 19:37:25 +0000

    Task2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
+
+fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`File ${filePath} written successfully!`);
+});
+
