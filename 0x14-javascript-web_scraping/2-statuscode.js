5b27325cd9b2d69ee9ab54a7fbf93490f5680c2d
Jacie1 <irinemwanga5@gmail.com>
2023-04-25 19:49:23 +0000

    task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,11 +4,10 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
+    
   }
-
   console.log(`code: ${response.statusCode}`);
 });
