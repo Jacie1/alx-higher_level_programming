2cbf78f5df4574945619d93ad5726fe0c28c937d
Jacie1 <irinemwanga5@gmail.com>
2023-04-25 20:00:40 +0000

    task3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
-
 const request = require('request');
 
 const movieId = process.argv[2];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve movie information. Status code: ${response.statusCode}`);
     return;
   }
 
   const movie = JSON.parse(body);
 
+  if (movie.episode_id !== Number(movieId)) {
+    console.error(`No movie found with ID ${movieId}`);
+    return;
+  }
+
   console.log(`Title of episode ${movie.episode_id}: ${movie.title}`);
 });
-
