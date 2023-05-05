ffa132a6e8486c8abdc96cbd4ff2cdd14b35f1e3
Jacie1 <irinemwanga5@gmail.com>
2023-05-05 13:26:51 +0000

    script 0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Get the header element using document.querySelector()
+const header = document.querySelector('header');
+
+// Update the text color of the header element to red
+header.style.color = '#FF0000';
