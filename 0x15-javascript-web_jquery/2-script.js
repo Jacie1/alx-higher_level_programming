aaca93165318b8c2621cd465111fac535486acb3
Jacie1 <irinemwanga5@gmail.com>
2023-05-05 13:38:57 +0000

    tasks

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Listen for a click event on the DIV#red_header element using jQuery
+$('#red_header').click(function() {
+  // Update the text color of the header element to red
+  $('header').css('color', '#FF0000');
+});
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// Listen for a click event on the DIV#red_header element using jQuery
+$('#red_header').click(function() {
+  // Add the class "red" to the header element
+  $('header').addClass('red');
+});
